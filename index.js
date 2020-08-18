8ef288b412083563f1f27391349dbfd11e3d8b50
Néville Dubuis <ndubuis@gmail.com>
2020-08-18 12:14:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const markdownIt = require('markdown-it');
+const md = new markdownIt();
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+app.use((req, res, next)=> {
+  console.log(req.originalUrl);
+  next();
+})
+
+app.get('/', function(req, res){
+  res.send('Hello World!');
+});
+
+app.post("/convert", function(req, res, next) {
+  console.log('converting');
+  if(typeof req.body == 'undefined' || req.body == null) {
+    res.json(["error", "No data found"]);
+  } else {
+
+    res.send(md.render(req.body));
+  }
+});
+
+app.listen(3000, "0.0.0.0", function() {
+ console.log("Server running on port 3000");
+});
